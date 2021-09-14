88d047ea157ef361fb08d38534183b5dac730504
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-09-14 17:36:29 +0900

    エラーメッセージの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,5 +16,7 @@ class Item < ApplicationRecord
   validates :shipping_charge_id, presence: true
   validates :prefecture_id, presence: true
   validates :shipping_day_id, presence: true
-  validates :price, presence: true, format: { with: /[300-9999999]\d/, message: " is out of setting range" }
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters" },
+  numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to:9999999, message: " is out of setting range"}  
+
 end
