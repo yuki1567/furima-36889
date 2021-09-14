c1273a07e3a11d8d665fccc1a5b9b4d13ec3bace
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-09-14 14:47:33 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,13 @@ class Item < ApplicationRecord
   belongs_to :shipping_charge
   belongs_to :prefecture
   belongs_to :shipping_day
+
+  validates :name, presence: true
+  validates :content, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :shipping_charge_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_day_id, presence: true
+  validates :price, presence: true
 end
