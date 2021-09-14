e6924a6c2f0ed3d87a58941f1afefed79301631c
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-09-14 20:28:19 +0900

    Fakerの設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    name               { Faker::Name.name }
+    content            { Faker::Lorem.sentence }
+    category_id        { Faker::Number.between(from: 1, to: 10) }
+    status_id          { Faker::Number.between(from: 1, to: 6) }
+    shipping_charge_id { Faker::Number.between(from: 1, to: 2) }
+    prefecture_id      { Faker::Number.between(from: 1, to: 47) }
+    shipping_day_id    { Faker::Number.between(from: 1, to: 3) }
+    price              { Faker::Number.between(from: 300, to: 9999999)}
+    associations :user
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
