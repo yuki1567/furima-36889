b0bbc63954a277d9b15cfb07b7df540ee8f4d67f
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-09-14 21:17:24 +0900

    itemモデルのテスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -7,8 +7,8 @@ FactoryBot.define do
     shipping_charge_id { Faker::Number.between(from: 1, to: 2) }
     prefecture_id      { Faker::Number.between(from: 1, to: 47) }
     shipping_day_id    { Faker::Number.between(from: 1, to: 3) }
-    price              { Faker::Number.between(from: 300, to: 9999999)}
-    associations :user
+    price              { Faker::Number.between(from: 300, to: 9_999_999) }
+    association :user
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
