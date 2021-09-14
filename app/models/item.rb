2eb2074d381017b31c8abd4a21709a01132c098c
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-09-14 19:54:52 +0900

    価格のバリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,7 +16,5 @@ class Item < ApplicationRecord
   validates :shipping_charge_id, presence: true
   validates :prefecture_id, presence: true
   validates :shipping_day_id, presence: true
-  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters" },
-  numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to:9999999, message: " is out of setting range"}  
-
+  validates :price, presence: true, inclusion: { in: 300..9999999, message: 'is out of setting range' }, numericality: { message: 'is invalid. Input half-width characters' }
 end
