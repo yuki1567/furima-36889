0d57442b725c520fee01bc77087f22e752d3ac19
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-09-14 14:25:41 +0900

    ActiveHashの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :shipping_day
 end
