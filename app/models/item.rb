e07fc6e8866c649ba0dd951763c4c367e50ce74d
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-09-14 16:10:50 +0900

    価格のバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,5 +16,5 @@ class Item < ApplicationRecord
   validates :shipping_charge_id, presence: true
   validates :prefecture_id, presence: true
   validates :shipping_day_id, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, format: { with: /[300-9999999]\d/, message: " is out of setting range" }
 end
