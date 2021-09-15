efab89fbe9a6ec432b2129405e4ce23125d09229
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-09-15 18:15:53 +0900

    Rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,9 +24,7 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    unless @item.user.id == current_user.id
-      redirect_to root_path
-    end
+    redirect_to root_path unless @item.user.id == current_user.id
   end
 
   def update
