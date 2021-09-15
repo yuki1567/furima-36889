65e0810cb9b404c4edb84506f4b40e08e3ce4de4
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-09-15 17:54:21 +0900

    updateアクションの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,8 +27,12 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
   end
 
   private
