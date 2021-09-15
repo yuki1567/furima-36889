43d516726f477cbb0b0a61bed502454f6ad41a31
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-09-15 20:21:20 +0900

    コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -52,6 +52,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
+    @item = Item.find(params[:id])
     redirect_to root_path unless @item.user.id == current_user.id
   end
 end
