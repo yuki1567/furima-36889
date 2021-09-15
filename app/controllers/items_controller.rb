635a1ef04c6a5120c58ae9b33f0c253d84d39526
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-09-15 17:28:28 +0900

    itemsコントローラーのedit,updateアクションの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,15 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+  end
+
   private
 
   def item_params
