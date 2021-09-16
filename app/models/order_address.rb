0cf9acfefe736bf7ff42f9fc3c4bf46798992221
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-09-16 15:26:50 +0900

    orderモデルとaddressモデルのバリデーション設定

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,4 +1,14 @@
 class OrderAddress
   include ActiveModel::Model
-  attar_accessor :item_id, :user_id, :postal_code, :prefecture_id, :municipalities, :street_address, :detail_address, :telephone, :order_id
+  attar_accessor :item_id, :user_id, :postal_code, :prefecture_id, :municipality, :street_address, :detail_address, :telephone
+
+  with_options presence: true do
+    validates :item_id
+    validates :user_id
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
+    validates :prefecture_id
+    validates :municipality
+    validates :street_address
+    validates :telephone, length: { minimum: 10, maximum: 11, message: "is too short" }, format: { with: /\A[0-9]+\z/, message: " is invalid. Input only number"}
+  end
 end
