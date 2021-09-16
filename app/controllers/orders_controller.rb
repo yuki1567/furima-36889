ae68c80f29e357554959baeda2a01d39edce85bb
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-09-16 21:50:10 +0900

    環境変数の設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -28,7 +28,7 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = "sk_test_c77ba5536bc69f1cb1ca9c23"
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item.price,
       card: order_params[:token],
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_9c81eef7d067150d99c8d0d2");
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
