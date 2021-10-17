14435cca3bb4773b65b1accc893a2a74dace5153
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-10-17 16:38:49 +0900

    画像プレビュー機能実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,12 +1,14 @@
 function itemPrice (){
-  const price = document.getElementById("item-price");
-  price.addEventListener("keyup", () => {
-    const priceVal = price.value;
-    const priceTax = document.getElementById("add-tax-price");
-    priceAfterTax = Math.floor(priceVal * 0.1);
-    priceTax.innerHTML = `${priceAfterTax}`;
-    const profit = document.getElementById("profit");
-    profit.innerHTML = `${priceVal - priceAfterTax}`; 
-  });
+  if (document.getElementById("item-price")){
+    const price = document.getElementById("item-price");
+    price.addEventListener("keyup", () => {
+      const priceVal = price.value;
+      const priceTax = document.getElementById("add-tax-price");
+      priceAfterTax = Math.floor(priceVal * 0.1);
+      priceTax.innerHTML = `${priceAfterTax}`;
+      const profit = document.getElementById("profit");
+      profit.innerHTML = `${priceVal - priceAfterTax}`; 
+    });
+  }
 };
 window.addEventListener('load', itemPrice);
---
+++ app/javascript/preview.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function(){
+  if (document.getElementById('item-image')){
+    const ImageList = document.getElementById('image-list');
+
+    const createImageHTML = (blob) => {
+      const imageElement = document.createElement('div');
+      const blobImage = document.createElement('img');
+      blobImage.className = "preview-item-img"
+      blobImage.setAttribute('src', blob);
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+    };
+
+    document.getElementById('item-image').addEventListener('change', function(e){
+      const imageContent = document.querySelector('img');
+      if (imageContent){
+        imageContent.remove();
+      }
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+
+      createImageHTML(blob);
+    });
+  }
+});
