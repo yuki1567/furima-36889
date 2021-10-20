180f8e50149dd8c7f67ca53e438e81a782e45278
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-10-20 16:15:50 +0900

    ActionCable本番環境へのデプロイの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  ActionCable.server.config.disable_request_forgery_protection = true
+  config.action_cable.url = "wss://furima-36889.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://furima-36889.herokuapp.com', 'http://furima-36889.herokuapp.com']
 end
