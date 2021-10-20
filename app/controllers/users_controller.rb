95ee8f16e6763a751038ea56ad3e24e7a859f9c8
yuki1567 <89760110+yuki1567@users.noreply.github.com>
2021-10-20 17:26:03 +0900

    マイページ機能修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
 
   def show 
     @user = User.find(params[:id])
+    redirect_to root_path unless @user.id == current_user.id
     if current_user.card.present?
       Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
       card = Card.find_by(user_id: current_user.id)
@@ -9,5 +11,4 @@ class UsersController < ApplicationController
       @card = customer.cards.first
     end
   end
-
 end
